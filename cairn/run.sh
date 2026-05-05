a1f969d475419b774e631c4c09aafb8e45354612
TheROcraft <136098318+TheROcraft@users.noreply.github.com>
2026-05-05 19:35:09 +0200

    Update run.sh

--- cairn/run.sh
+++ cairn/run.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
+
 export CAIRN_ENCRYPTION_KEY=$(jq --raw-output '.CAIRN_ENCRYPTION_KEY' /data/options.json)
 
-echo "--- Cairn Add-on wird gestartet ---"
-
+echo "Starting Cairn Backend..."
 exec /usr/local/bin/cairn
